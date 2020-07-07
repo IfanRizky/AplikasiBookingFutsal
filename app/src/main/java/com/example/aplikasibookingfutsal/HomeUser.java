507ceb7f0c2a434b67fc5f3303bb8de46f49186b
Mochammad Ifan Rizky Kurniawan <rizkykurniawan.ifan463@gmail.com>
2020-07-07 23:02:38 +0700

    add code to change botnavbar in HomeUser

--- app/src/main/java/com/example/aplikasibookingfutsal/HomeUser.java
+++ app/src/main/java/com/example/aplikasibookingfutsal/HomeUser.java
@@ -1,8 +1,13 @@
 package com.example.aplikasibookingfutsal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeUser extends AppCompatActivity {
 
@@ -10,5 +15,41 @@ public class HomeUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_user);
+
+        //Initialize and Assign Varible
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_user);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.homeuser);
+
+        //Perform SwlectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.booking:
+                        startActivity(new Intent(getApplicationContext(),
+                                Booking.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.homeuser:
+                        return true;
+
+                    case R.id.paymentuser:
+                        startActivity(new Intent(getApplicationContext(),
+                                PaymentUser.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.profileuser:
+                        startActivity(new Intent(getApplicationContext(),
+                                ProfileUser.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
