2580f0fba40717f9e644d09e532e89e16627358f
Mochammad Ifan Rizky Kurniawan <rizkykurniawan.ifan463@gmail.com>
2020-07-07 23:01:00 +0700

    add code tu change botnavbar in HomeAdmin

--- app/src/main/java/com/example/aplikasibookingfutsal/HomeAdmin.java
+++ app/src/main/java/com/example/aplikasibookingfutsal/HomeAdmin.java
@@ -1,8 +1,13 @@
 package com.example.aplikasibookingfutsal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeAdmin extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class HomeAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_admin);
+
+        //Initialize and Assign Varible
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_adm);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.homeadmin);
+
+        //Perform SwlectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.homeadmin:
+                        return true;
+
+                    case R.id.paymentadmin:
+                        startActivity(new Intent(getApplicationContext(),
+                                PaymentAdmin.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.profileadmin:
+                        startActivity(new Intent(getApplicationContext(),
+                                ProfileAdmin.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
