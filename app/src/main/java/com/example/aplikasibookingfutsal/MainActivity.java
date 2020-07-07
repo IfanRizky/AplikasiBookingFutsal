1311c85a9cf4e2749f8c0875e5ef580544cb90b6
Mochammad Ifan Rizky Kurniawan <rizkykurniawan.ifan463@gmail.com>
2020-07-07 22:10:12 +0700

    add change main_menu in User

--- app/src/main/java/com/example/aplikasibookingfutsal/MainActivity.java
+++ app/src/main/java/com/example/aplikasibookingfutsal/MainActivity.java
@@ -2,9 +2,42 @@ package com.example.aplikasibookingfutsal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.item1: {
+                Toast.makeText(getApplicationContext(), "Ini adalah tampilan User",
+                        Toast.LENGTH_LONG).show();
+            }break;
+
+            case  R.id.item2: {
+                Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(i);
+                Toast.makeText(getApplicationContext(), "Ini adalah tampilan Admin",
+                        Toast.LENGTH_LONG).show();
+            }break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
