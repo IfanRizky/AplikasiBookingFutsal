8ae73b3a489dbe16cf4a251a15a979dffd82e569
Mochammad Ifan Rizky Kurniawan <rizkykurniawan.ifan463@gmail.com>
2020-07-07 22:06:47 +0700

    add some activities

---
+++ app/src/main/java/com/example/aplikasibookingfutsal/PaymentAdmin.java
@@ -0,0 +1,14 @@
+package com.example.aplikasibookingfutsal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_admin);
+    }
+}
---
+++ app/src/main/java/com/example/aplikasibookingfutsal/PaymentUser.java
@@ -0,0 +1,14 @@
+package com.example.aplikasibookingfutsal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_user);
+    }
+}
