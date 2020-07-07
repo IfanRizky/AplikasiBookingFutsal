2a19adc68f33fb07a79bec1c42ac89d7085188cf
Mochammad Ifan Rizky Kurniawan <rizkykurniawan.ifan463@gmail.com>
2020-07-07 22:20:08 +0700

    add change main_menu in admin

--- app/src/main/java/com/example/aplikasibookingfutsal/Main2Activity.java
+++ app/src/main/java/com/example/aplikasibookingfutsal/Main2Activity.java
@@ -2,9 +2,38 @@ package com.example.aplikasibookingfutsal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.item1: {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                Toast.makeText(getApplicationContext(), "Ini adalah tampilan User",
+                        Toast.LENGTH_LONG).show();
+            }break;
+
+            case  R.id.item2: {
+                Toast.makeText(getApplicationContext(), "Ini adalah tampilan Admin",
+                        Toast.LENGTH_LONG).show();
+            }break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
